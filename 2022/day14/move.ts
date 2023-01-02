e9e826ef5be2ad586866485dfb82170e91e451e6
Herve07h22 <contact@camilab.co>
2023-01-02 15:04:38 +0100

    simplified day14

--- 2022/day14/move.ts
+++ 2022/day14/move.ts
@@ -1,77 +1,76 @@
 import {
-    sandSourcePosition,
-    Position,
-    samePosition,
-    straightFall,
-    leftFall,
-    rightFall,
-    dontMove
+  sandSourcePosition,
+  Position,
+  samePosition,
+  straightFall,
+  leftFall,
+  rightFall,
+  dontMove,
 } from "./Position";
 
 type State = {
-    rocksAndSandMap: string[][];
-    position: Position;
-    nbOfFallenSand: number;
-    gameOver: boolean;
+  rocksAndSandMap: string[][];
+  position: Position;
+  nbOfFallenSand: number;
+  gameOver: boolean;
 };
 
 export function simulatePouringSand(rocksAndSandMap: string[][]) {
-    var state: State = {
-        rocksAndSandMap,
-        position: sandSourcePosition,
-        nbOfFallenSand: 0,
-        gameOver: false,
-    };
-    while (!state.gameOver) {
-        const nextState = move(state);
-        state = nextState;
-    }
-    return state;
+  var state: State = {
+    rocksAndSandMap,
+    position: sandSourcePosition,
+    nbOfFallenSand: 0,
+    gameOver: false,
+  };
+  while (!state.gameOver) {
+    const nextState = move(state);
+    state = nextState;
+  }
+  return state;
 }
 
 export function move(state: State): State {
-    const { rocksAndSandMap, position, nbOfFallenSand, gameOver } = state;
-    const sizeX = rocksAndSandMap.length;
-    const sizeY = rocksAndSandMap[0].length;
-    const updatedState = (movement: (p: Position) => Position) => ({
-        rocksAndSandMap,
-        position: movement(position),
-        nbOfFallenSand,
-        gameOver: gameOver || samePosition(movement(position), position),
-    });
+  const { rocksAndSandMap, position, nbOfFallenSand, gameOver } = state;
+  const sizeX = rocksAndSandMap.length;
+  const sizeY = rocksAndSandMap[0].length;
+  const updatedState = (movement: (p: Position) => Position) => ({
+    rocksAndSandMap,
+    position: movement(position),
+    nbOfFallenSand,
+    gameOver: gameOver || samePosition(movement(position), position),
+  });
 
-    // Game over ?
-    if (position.y + 1 === sizeY ||
-        position.x + 1 === sizeX ||
-        (samePosition(position, sandSourcePosition) &&
-            rocksAndSandMap[position.x][position.y] !== "+")) {
-        return updatedState(dontMove);
-    }
+  // Out of range or full = Game over
+  if (
+    position.y + 1 === sizeY ||
+    position.x + 1 === sizeX ||
+    (samePosition(position, sandSourcePosition) &&
+      rocksAndSandMap[position.x][position.y] !== "+")
+  ) {
+    return updatedState(dontMove);
+  }
 
-    // Straight fall
-    if (position.y + 1 < sizeY &&
-        rocksAndSandMap[position.x][position.y + 1] === ".") {
-        return updatedState(straightFall);
-    }
+  // Straight fall
+  if (rocksAndSandMap[position.x][position.y + 1] === ".") {
+    return updatedState(straightFall);
+  }
 
-    // left available
-    if (position.x - 1 >= 0 &&
-        rocksAndSandMap[position.x - 1][position.y + 1] === ".") {
-        return updatedState(leftFall);
-    }
+  // left available
+  if (rocksAndSandMap[position.x - 1][position.y + 1] === ".") {
+    return updatedState(leftFall);
+  }
 
-    // right available
-    if (position.x + 1 < sizeX &&
-        rocksAndSandMap[position.x + 1][position.y + 1] === ".") {
-        return updatedState(rightFall);
-    }
+  // right available
+  if (rocksAndSandMap[position.x + 1][position.y + 1] === ".") {
+    return updatedState(rightFall);
+  }
 
-    rocksAndSandMap[position.x][position.y] = "o";
+  rocksAndSandMap[position.x][position.y] = "o";
 
-    return {
-        rocksAndSandMap,
-        position: sandSourcePosition,
-        nbOfFallenSand: nbOfFallenSand + 1,
-        gameOver: false,
-    };
+  return {
+    rocksAndSandMap,
+    position: sandSourcePosition,
+    nbOfFallenSand: nbOfFallenSand + 1,
+    gameOver: false,
+  };
 }
